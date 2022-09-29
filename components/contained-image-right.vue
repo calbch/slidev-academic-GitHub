60aa28753fe39316f1f044a10bfea7640704c30d
cal <caldotio@protonmail.com>
2022-09-29 15:10:14 +0200

    added components

---
+++ components/contained-image-right.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "../node_modules/slidev-theme-academic/layout-helper";
+
+const props = defineProps({
+  image: {
+    type: String,
+  },
+  class: {
+    type: String,
+  },
+});
+const style = computed(() => handleBackground(props.image));
+</script>
+
+<template>
+  <div class="grid grid-cols-2 w-full h-full pr-4">
+    <div class="slidev-layout default" :class="props.class">
+      <slot />
+    </div>
+    <div class="w-full w-full bg" :style="style" />
+  </div>
+</template>
+
+<style scoped>
+.bg {
+    background-size: contain !important;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+}
+
+</style>
---
+++ components/two-cols-one-heading.vue
@@ -0,0 +1,47 @@
+
+<!--
+  Usage:
+
+```md
+---
+layout: two-cols-one-heading
+---
+
+# Heading
+
+::left::
+
+# Left
+
+This shows on the left
+
+::right::
+
+# Right
+
+This shows on the right
+```
+
+-->
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout">
+    <slot />
+    <div class="two-columns w-full h-full grid grid-cols-2 space-x-8">
+      <div class="col-left" :class="props.class">
+        <slot name="left"/>
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
